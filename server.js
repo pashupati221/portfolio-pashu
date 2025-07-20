c56f88a3a85d49f39d3a5835a27de96d8575af04
pashupati221 <pashupati.wd.bca.22@gmail.com>
2025-07-20 13:29:46 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,34 +1,72 @@
+// const express = require("express");
+// const fs =require("fs");
+// const app = express();
+// const port =4000;
+
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.static(${__dirname}))
+// app.listen(port,()=>{
+//     console.log(server started http://localhost:${port})})
+// app.get("/", (req, res) => {
+//     res.sendFile("index.html")
+// })
+// app.post("/",(req,res)=>{
+//     const Name =req.body.name;
+//     const Email =req.body.email;
+//     const subject =req.body.subject;
+//     const message =req.body.message;
+    
+//     fs.appendFile("file.txt",`Name :${Name}  
+//      email:${Email}
+//      subject:${subject}
+//     message:${message}`,"utf-8",(err)=>{console.log(err)
+//     });
+    
+// })
+
+
 const express = require("express");
-const fs =require("fs");
+const fs = require("fs");
+const path = require("path");
 const app = express();
-const port =4000;
+const port = 4000;
 
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.static(${__dirname}))
-app.listen(port,()=>{
-    console.log(server started http://localhost:${port})
-        })
-app.get("/", (req, res) => {
-    res.sendFile("index.html")
-})
-app.post("/",(req,res)=>{
-    const Name =req.body.name;
-    const Email =req.body.email;
-    const subject =req.body.subject;
-    const message =req.body.message;
-    
-    fs.appendFile("file.txt",`Name :${Name}  
-     email:${Email}
-     subject:${subject}
-    message:${message}
-    
-    
-    `
+app.use(express.urlencoded({ extended: true }));
 
-    ,"utf-8",
-    (err)=>{
-        console.log(err)
+// Serve static files
+app.use(express.static(__dirname));
+
+// GET route to serve index.html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// POST route to handle form data
+app.post("/", (req, res) => {
+    const { name, email, subject, message } = req.body;
+
+    const data = `
+Name: ${name}
+Email: ${email}
+Subject: ${subject}
+Message: ${message}
+--------------------------\n`;
+
+    fs.appendFile("file.txt", data, "utf-8", (err) => {
+        if (err) {
+            console.error("Error writing to file:", err);
+            res.status(500).send("Internal Server Error");
+        } else {
+            console.log("Data saved to file.txt");
+            res.send("Form submitted successfully!");
+        }
     });
-    
-})
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
